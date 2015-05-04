eb7e691cefdd57df84aa098fc9ead8c6a76f0dd2
Dennis Christoffersen <dennis@fantastisk.dk>
2015-05-04 16:27:16 +0200

    adding primus files and static content

--- primusSetup.js
+++ primusSetup.js
@@ -1,63 +1,46 @@
 
 // IMPORTS
-var Primus = require('primus')
-var primusEmitPlugin = require('primus-emit')
-var ip = require ('ip')
+var Primus = require('primus');
+var ip = require ('ip');
+var os = require ('os');
 
-
-var hostName = os.hostname();
-console.log (" primusFan.js > hostName " , hostName)
-if(hostName == "Fathead" || hostName == "Fathead-PC"  ){
-
-} else{
-    hostName = ""
-}
-
-var primus = null;
-
-// INIT --------------------------------
-exports.init = function (server, redisClient, port) {
-
-    // saving scope
-    var module = this;
-
-// SETUP PRIMUS - WITH OPTIONS
+// Vars
 //---------------------------------------------------------------------------------------
 
-    var namespace = 'metroplex' + hostName;
+var hostName = os.hostname();
+var primus = null;
+var primusAdresse = "";
+var namespace = "";
 
-    var SERVO_ID = ""
-    if(process.env.SERVO_ID != undefined ){
-        SERVO_ID = process.env.SERVO_ID
-    }
+// INIT
+//---------------------------------------------------------------------------------------
+exports.init = function (server, redisClient, port) {
 
-    var primusAdresse = "";
+// SETUP locally
+//---------------------------------------------------------------------------------------
 
-    if(SERVO_ID != "" ){
-        primusAdresse = "http://fan.mod.bz" + "?x-mod-servo=" +SERVO_ID
-    }else{
-        primusAdresse = "http://"+ip.address()+":"+port
-    }
+    primusAdress = "http://"+ip.address()+":"+port;
+    namespace = 'metroplex' + 'localtest';
+    console.log (" primusSetup.js > primusAdresse = " , primusAdress);
 
 
-    console.log (" primusFan.js > primusAdresse " , primusAdresse)
+// SETUP PRIMUS
+//---------------------------------------------------------------------------------------
 
     primus = new Primus(server ,{
         parser: 'JSON',
-        transformer: 'engine.io',
+        transformer: 'faye',
         namespace: namespace,
         redis:redisClient,
-        address:primusAdresse,
-        url:primusAdresse+"/primus/omega/supreme"
+        address:primusAdress,
+        url:primusAdress+"/primus/omega/supreme"
     });
-
 
 
 // REGISTER THE PLUGINS
 //---------------------------------------------------------------------------------------
     primus.use('metroplex', require('metroplex'));
     primus.use('omega-supreme', require('omega-supreme'));
-    primus.use('emit', primusEmitPlugin);
 
 
 
@@ -68,49 +51,31 @@ exports.init = function (server, redisClient, port) {
         console.log (" primusFan.js > LIB SAVED = " );
     });
 
+
 // USER CONNECTS
 //---------------------------------------------------------------------------------------
 
-    primus.on('connection', function (sp) {
-        var spark = sp;
+    primus.on('connection', function (spark) {
 
-        spark.on('testdata', function message(data) {
-            module.testServers(spark)
-        });
+        spark.on('data', function message(data) {
+            primus.metroplex.servers(function (err, servers) {
+                console.log (" primusSetup.js > servers  = " , servers );
 
-    });// close connection
-
-
-    primus.on('disconnection', function (spark) {
-        console.log (" primusFan.js > USER DISCONNECTED = ");
-    });
-
-}; // Close init
-
-
-
-// GET CONNNECTED USERS
-//---------------------------------------------------------------------------------------
-
-exports.testServers = function (spark) {
-
-    primus.metroplex.servers(function (err, servers) {
-        console.log("not all = ", servers);
-        console.log (" primusFan.js > ------------------------------------------");
-    });
-
-    primus.metroplex.servers(function (err, servers) {
-
-        servers.forEach(function (server) {
-            primus.forward(server, {
-                emit: [ "testEvent", "from server" ]
-
-            }, function (err, data) {
-                console.log (" primusFan.js > err, data = " , err, data);
-
+                servers.forEach(function (server) {
+                    primus.forward(server,
+                           data
+                        ,
+                        function (err, data) {
+                            console.log (" primusFan.js > err, data = " , err, data);
+                        });
+                });
             });
         });
-    });
+    });// close connection
 
-};
+}; // close init
+
+
+
+
 
--- public/main.js
+++ public/main.js
@@ -1,3 +1,19 @@
 /**
  * Created by Fantastisk on 04-05-2015.
  */
+
+
+var btn = document.getElementById("btn")
+var primus = new Primus();
+
+btn.onclick = function(){
+    primus.write("Send to server");
+};
+
+primus.on('data', function received(data) {
+    toastr.info('data = ', data)
+});
+
+primus.on('open', function received(data) {
+    toastr.info('open')
+});
